35971fe42b966f72ad1b968c4b416eeffe42e7eb
rub3z <rub3z@users.noreply.github.com>
2019-10-29 15:44:34 -0700

    I should have put this in sometime earlier. Better than never! Don't want to be a jerk, after all.

--- cplusplusGameExperimental/cplusplusGameExperimental/AABBTree.cpp
+++ cplusplusGameExperimental/cplusplusGameExperimental/AABBTree.cpp
@@ -1,3 +1,31 @@
+/*
+  Copyright (c) 2009 Erin Catto http://www.box2d.org
+  Copyright (c) 2019 Ruben Baerga-Ortega https://github.com/rub3z
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+  This code was adapted from parts of the Box2D Physics Engine,
+  http://www.box2d.org
+  WITH SPECIAL THANKS TO:
+  Nathanael Presson
+  https://github.com/bulletphysics/bullet3/blob/master/src/BulletCollision/BroadphaseCollision/btDbvt.cpp
+  James Randall
+  https://www.azurefromthetrenches.com/introductory-guide-to-aabb-tree-collision-detection/
+  https://github.com/JamesRandall/SimpleVoxelEngine
+  Lester Hedges
+  https://github.com/lohedges/aabbcc
+*/
+
 #include "stdafx.h"
 #include "AABBTree.h"
 
